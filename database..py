20723414af9abae8187623066dad323031c41771
souma125 <53054888+souma125@users.noreply.github.com>
2023-09-09 20:33:05 +0530

    new files added

---
+++ database..py
@@ -0,0 +1,6 @@
+from sqlalchemy import create_engine,text
+engine = create_engine("mysql+pymysql://root:@localhost/parineetas_career?charset=utf8mb4")
+with engine.connect() as connection:
+    result = connection.execute(text("SELECT title FROM `jobs`"))
+    for row in result:
+        print("title:", row.title)
--- test.py
+++ test.py
@@ -1,7 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,jsonify
 app = Flask(__name__)
+JOBS = [
+    {
+        'id': 1,
+        'title':'Django Devloper',
+        'location': 'Kolkata',
+        'Salary': 'Rs. 100000'
+    },
+    {
+        'id': 2,
+        'title':'Python Devloper',
+        'location': 'Bangaluru',
+        'Salary': 'Rs. 100000'
+    },
+    {
+        'id': 3,
+        'title':'AI Devloper',
+        'location': 'Pune',
+        'Salary': 'Rs. 200000'
+    }
+]
 @app.route('/')
 def hello():
-    return render_template('home.html')
+    return render_template('home.html',jobs=JOBS, company_name = 'Parineeta')
+
+@app.route('/jobs')
+def jobs():
+    return jsonify(JOBS)
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=True)
